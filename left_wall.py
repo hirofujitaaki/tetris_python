611d578525e7ade8ed8a27563091e441cb7645fa
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-02 15:29:10 +0900

    complete left-wall

---
+++ left_wall.py
@@ -0,0 +1,5 @@
+l_wall_times = 0
+while l_wall_times < 20:
+
+
+    l_wall_times = l_wall_times + 1
--- tetris.py
+++ tetris.py
@@ -5,7 +5,7 @@ import pygame
 def run_game():
     # Initialize the game and create a screen object.
     pygame.init()
-    screen = pygame.display.set_mode((400, 400))
+    screen = pygame.display.set_mode((280, 280))
     pygame.display.set_caption("Tetris")
     black = (0, 0, 0)
     white = (255, 255, 255)
@@ -13,21 +13,25 @@ def run_game():
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit(); sys.exit()
+                pygame.quit()
+                sys.exit()
 
         screen.fill(black)
 
-        y_times = 0
-        y_position = 199
-        while y_times < 12:
-            x_times = 0
-            x_position = 199
-            while x_times < 12:
-                pygame.draw.rect(screen, white, (x_position, y_position, 1, 1,))
-                x_position = x_position + 1
-                x_times = x_times + 1
-            y_position = y_position + 1
-            y_times = y_times + 1
+        l_wall_times = 0
+        while l_wall_times < 20:
+            y_times = 0
+            y_position = 0 + l_wall_times * 14
+            while y_times < 12:
+                x_times = 0
+                x_position = 0
+                while x_times < 12:
+                    pygame.draw.rect(screen, white, (x_position, y_position, 1, 1,))
+                    x_position = x_position + 1
+                    x_times = x_times + 1
+                y_position = y_position + 1
+                y_times = y_times + 1
+            l_wall_times = l_wall_times + 1
 
         # update a portion of screen.
         pygame.display.update()
