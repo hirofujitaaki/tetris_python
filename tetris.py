8aac81936e8d5759e8ecc539e2b3f0bb71b26afc
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-02 16:03:55 +0900

    l-, r-walls and bottom

--- tetris.py
+++ tetris.py
@@ -33,6 +33,36 @@ def run_game():
                 y_times = y_times + 1
             l_wall_times = l_wall_times + 1
 
+        r_wall_times = 0
+        while r_wall_times < 20:
+            y_times = 0
+            y_position = 0 + r_wall_times * 14
+            while y_times < 12:
+                x_times = 0
+                x_position = 154
+                while x_times < 12:
+                    pygame.draw.rect(screen, white, (x_position, y_position, 1, 1))
+                    x_position = x_position + 1
+                    x_times = x_times + 1
+                y_position = y_position + 1
+                y_times = y_times + 1
+            r_wall_times = r_wall_times + 1
+
+        bottom_times = 0
+        while bottom_times < 10:
+            x_times = 0
+            x_position = 14 + bottom_times * 14
+            while x_times < 12:
+                y_times = 0
+                y_position = 266
+                while y_times < 12:
+                    pygame.draw.rect(screen, white, (x_position, y_position, 1, 1))
+                    y_position = y_position + 1
+                    y_times = y_times + 1
+                x_position = x_position + 1
+                x_times = x_times + 1
+            bottom_times = bottom_times + 1
+
         # update a portion of screen.
         pygame.display.update()
 
