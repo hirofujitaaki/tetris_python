32ab9780a809cf437925f8161ffb42a24df97e62
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-02 13:24:34 +0900

    complete chapter3

---
+++ tetris.py
@@ -0,0 +1,35 @@
+import sys
+import pygame
+
+
+def run_game():
+    # Initialize the game and create a screen object.
+    pygame.init()
+    screen = pygame.display.set_mode((400, 400))
+    pygame.display.set_caption("Tetris")
+    black = (0, 0, 0)
+    white = (255, 255, 255)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit(); sys.exit()
+
+        screen.fill(black)
+
+        y_times = 0
+        y_position = 199
+        while y_times < 12:
+            x_times = 0
+            x_position = 199
+            while x_times < 12:
+                pygame.draw.rect(screen, white, (x_position, y_position, 1, 1,))
+                x_position = x_position + 1
+                x_times = x_times + 1
+            y_position = y_position + 1
+            y_times = y_times + 1
+
+        # update a portion of screen.
+        pygame.display.update()
+
+run_game()
