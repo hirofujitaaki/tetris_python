619b57b3873f2690b0fe3f9a284899ef54d61eef
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-03 10:43:45 +0900

    complete chapter 4

--- tetris.py
+++ tetris.py
@@ -18,36 +18,37 @@ def run_game():
 
         screen.fill(black)
 
-        l_wall_times = 0
-        while l_wall_times < 20:
+        # wall on the left.
+        wall_times = 0
+        while wall_times < 20:
             y_times = 0
-            y_position = 0 + l_wall_times * 14
+            y_position = 0 + wall_times * 14
             while y_times < 12:
                 x_times = 0
                 x_position = 0
                 while x_times < 12:
                     pygame.draw.rect(screen, white, (x_position, y_position, 1, 1,))
+                    pygame.display.update()
                     x_position = x_position + 1
                     x_times = x_times + 1
                 y_position = y_position + 1
                 y_times = y_times + 1
-            l_wall_times = l_wall_times + 1
 
-        r_wall_times = 0
-        while r_wall_times < 20:
             y_times = 0
-            y_position = 0 + r_wall_times * 14
+            y_position = 0 + wall_times * 14
             while y_times < 12:
                 x_times = 0
                 x_position = 154
                 while x_times < 12:
                     pygame.draw.rect(screen, white, (x_position, y_position, 1, 1))
+                    pygame.display.update()
                     x_position = x_position + 1
                     x_times = x_times + 1
                 y_position = y_position + 1
                 y_times = y_times + 1
-            r_wall_times = r_wall_times + 1
+            wall_times = wall_times + 1
 
+        # wall on the bottom.
         bottom_times = 0
         while bottom_times < 10:
             x_times = 0
@@ -57,6 +58,7 @@ def run_game():
                 y_position = 266
                 while y_times < 12:
                     pygame.draw.rect(screen, white, (x_position, y_position, 1, 1))
+                    pygame.display.update()
                     y_position = y_position + 1
                     y_times = y_times + 1
                 x_position = x_position + 1
@@ -64,6 +66,5 @@ def run_game():
             bottom_times = bottom_times + 1
 
         # update a portion of screen.
-        pygame.display.update()
 
 run_game()
