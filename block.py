19d1e86544a223c96c8d3baf1f53ae6311eb6105
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-10 14:33:13 +0900

    chapter9 done

--- block.py
+++ block.py
@@ -6,23 +6,6 @@ class Block():
     def __init__(self, screen):
         self.screen = screen
 
-    # def squares(self, x_block_number, y_block_number, color, horizontal_blocks=0, vertical_blocks=0):
-    #     """draw a certain number of squares. """
-    #     y_times = 0
-    #     # Increase every 14 pixels in the y-axis.
-    #     y_block_num = y_block_number * 14 + vertical_blocks * 14
-    #     # Increase every 14 pixels in the x-axis.
-    #     x_block_num = x_block_number * 14 + horizontal_blocks * 14
-    #     while y_times < 12:
-    #         x_times = 0
-    #         x_blk_num = x_block_num
-    #         while x_times < 12:
-    #             pygame.draw.rect(self.screen, color, (x_blk_num, y_block_num, 1, 1))
-    #             x_blk_num = x_blk_num + 1
-    #             x_times = x_times + 1
-    #         y_block_num = y_block_num + 1
-    #         y_times = y_times + 1
-
     def squares(self, x_base_position, y_base_position, color, x_recurring=0, y_recurring=0):
         """draw a square for recurring times."""
         # Increase every 14 pixels in the y-axis.
--- tetris.py
+++ tetris.py
@@ -14,12 +14,14 @@ def main():
 
     x = 5
     y = 1
+    t = 0
     while True:
         # draw walls.
         settings.draw_walls(block)
         # draw bottom.
         settings.draw_bottom(block)
 
+        # paint it red.
         block.squares(x, y, settings.red)
         pygame.display.update()
 
@@ -29,16 +31,21 @@ def main():
                 sys.exit()
 
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
+                if event.key == pygame.K_RIGHT and x < 10:
                     block.squares(x, y, settings.black)
                     x += 1
 
-                elif event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT and x > 1:
                     block.squares(x, y, settings.black)
                     x += -1
 
+        # paint it back to black.
         block.squares(x, y, settings.black)
-        y += 1
+
+        # adjust the speed of falling blocks.
+        t += 1
+        if t % 10 == 0 and y < 18:
+            y += 1
 
 
 if __name__ == '__main__':
