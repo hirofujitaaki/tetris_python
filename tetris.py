ab31f5de721e867049250e34b5a4441c1be123be
hiforujitaaki <hirofujitaaki.gmail.com>
2017-01-15 14:46:43 +0900

    til 12.3

--- tetris.py
+++ tetris.py
@@ -2,7 +2,6 @@ import sys
 import pygame
 from block import Block
 from settings import Settings
-import test_setup
 
 
 def main():
@@ -14,20 +13,36 @@ def main():
     macro = {}  # to check collisions.
     block = Block(screen, macro)
     settings = Settings(screen)
+    settings.draw_walls(block)
+    settings.draw_bottom(block)
+
     x = 5
     y = 1
     t = 0
-    test_setup.slide(block, settings)  # for testing purpose.
     while True:
-        # draw walls.
-        settings.draw_walls(block)
-        # draw bottom.
-        settings.draw_bottom(block)
+
+        # x1 = x
+        # y1 = y
+        # if r == 0:
+        #     x1 = x + 1
+        # elif r == 1:
+        #     y1 = y + 1
+        # elif r == 2:
+        #     x1 = x - 1
+        # elif r == 3:
+        #     y1 = y - 1
 
         # draw the active block in red.
         block.square(x, y, settings.red)
+        block.square(x+1, y, settings.red)
+
         pygame.display.update()
 
+        # paint it back to black.
+        block.square(x, y, settings.black)
+        block.square(x+1, y, settings.black)
+
+        x_move = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -35,33 +50,35 @@ def main():
 
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    if block.check_macro(x+1, y) is False:
-                        block.square(x, y, settings.black)
-                        x += 1
+                    x_move = 1
 
                 elif event.key == pygame.K_LEFT:
-                    if block.check_macro(x-1, y) is False:
-                        block.square(x, y, settings.black)
-                        x += -1
+                    x_move = -1
 
-        # paint it back to black.
-        block.square(x, y, settings.black)
+                # elif event.key == pygame.K_UP:
+                #     r = r + 1
+                #     if r == 4:
+                #         r = 0
 
-        # adjust the speed of falling blocks.
+        x += x_move
+        if block.check_macro(x, y) or block.check_macro(x+1, y) is True:
+            x = x - x_move
+
         t += 1
-        if t % 10 == 0:
-            # check collisions.
-            if block.check_macro(x, y+1) is True:
+        if t % 10 == 0:  # adjust the speed of falling blocks.
+            y += 1
+
+            if block.check_macro(x, y) or block.check_macro(x+1, y) is True:
+                y = y - 1
                 block.square(x, y, settings.white)
+                block.square(x+1, y, settings.white)
                 block.fill_macro(x, y)
+                block.fill_macro(x+1, y)
                 if block.is_removeable(y):
                     block.remove_n_clear(y, settings.black)
                     block.slide_n_update(y, settings.white, settings.black)
                 x = 5
                 y = 1
-            else:
-                # keep falling down if no collisions.
-                y += 1
 
 
 if __name__ == '__main__':
