27fd457bb5c7e3bcbecb4b125f17b21c88125c3b
hiforujitaaki <hirofujitaaki.gmail.com>
2017-07-03 14:23:26 +0900

    bug fix: randint() to randrange()

--- settai_tetris/block.py
+++ settai_tetris/block.py
@@ -36,3 +36,79 @@ class Block():
             self.fill_macro(hori_block_num, 19)
 
             hori_block_num = hori_block_num + 1
+
+    def check_macro(self, x, y):
+        try:
+            return self.macro[(x, y)]
+        except KeyError:
+            return False
+
+    def is_removable(self, y):
+        x = 1
+        removable = True
+        while x < 11:
+            removable = removable and self.check_macro(x, y)
+            x += 1
+        return removable
+
+    def _clear_macro(self, x, y):
+        self.macro[(x, y)] = False
+
+    def remove_n_clear(self, y, color):
+        """ remove squares in a row and set the macro False. """
+        x = 1
+        while x < 11:
+            self.square(x, y, color)  # black only
+            self._clear_macro(x, y)
+            x += 1
+
+    def slide_n_update(self, y, color_w, color_b):
+        """ slide down all squares above and update the macro. """
+        y = y - 1
+        while y >= 0:
+            x = 1
+            while x <= 10:
+                if self.check_macro(x, y):
+                    self.square(x, y, color_b)
+                    self._clear_macro(x, y)
+                    self.square(x, y+1, color_w)
+                    self.fill_macro(x, y+1)
+                x += 1
+            y -= 1
+
+    def figure_sub_squares(self, x, y, r, typ):
+        if typ == 0:
+            base = {'x1': -1, 'y1': 0, 'x2': 1, 'y2': 0, 'x3': 2, 'y3': 0}
+            self._relative_positions(x, y, r, base)
+        elif typ == 1:
+            base = {'x1': -1, 'y1': -1, 'x2': -1, 'y2': 0, 'x3': 1, 'y3': 0}
+            self._relative_positions(x, y, r, base)
+        elif typ == 2:
+            base = {'x1': -1, 'y1': 0, 'x2': 0, 'y2': -1, 'x3': 1, 'y3': 0}
+            self._relative_positions(x, y, r, base)
+        elif typ == 3:
+            base = {'x1': -1, 'y1': 0, 'x2': 1, 'y2': -1, 'x3': 1, 'y3': 0}
+            self._relative_positions(x, y, r, base)
+        elif typ == 4:
+            base = {'x1': -1, 'y1': 0, 'x2': 0, 'y2': 1, 'x3': 1, 'y3': 1}
+            self._relative_positions(x, y, r, base)
+        elif typ == 5:
+            base = {'x1': 1, 'y1': 0, 'x2': 0, 'y2': 1, 'x3': 1, 'y3': 1}
+            self._relative_positions(x, y, r, base)
+        elif typ == 6:
+            base = {'x1': 1, 'y1': 0, 'x2': -1, 'y2': 1, 'x3': 0, 'y3': 1}
+            self._relative_positions(x, y, r, base)
+
+    def _relative_positions(self, x, y, r, base):
+        self.base = base
+        while (r + 4) % 4 > 0:
+            self.base['x1'], self.base['y1'] = self.base['y1'] * -1, self.base['x1']
+            self.base['x2'], self.base['y2'] = self.base['y2'] * -1, self.base['x2']
+            self.base['x3'], self.base['y3'] = self.base['y3'] * -1, self.base['x3']
+            r -= 1
+        self.base['x1'] += x
+        self.base['y1'] += y
+        self.base['x2'] += x
+        self.base['y2'] += y
+        self.base['x3'] += x
+        self.base['y3'] += y
--- settai_tetris/settai_tetris.py
+++ settai_tetris/settai_tetris.py
@@ -16,13 +16,95 @@ def main():
     block.draw_walls(settings.white)
     block.draw_bottom(settings.white)
 
+    x = 5
+    y = 1
+    t = 0
+    r = 0
+    typ = random.randrange(0, 7)
     while True:
+        x_move = 0
+        r_move = 0
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    x_move = 1
+
+                elif event.key == pygame.K_LEFT:
+                    x_move = -1
+
+                elif event.key == pygame.K_UP:
+                    r_move = 1
+
+        x += x_move
+        r += r_move
+        block.figure_sub_squares(x, y, r, typ)
+        if block.check_macro(x, y) or \
+                block.check_macro(block.base['x1'], block.base['y1']) or \
+                block.check_macro(block.base['x2'], block.base['y2']) or \
+                block.check_macro(block.base['x3'], block.base['y3']) is True:
+            if x_move != 0:
+                x -= x_move
+            elif r_move != 0:
+                r -= r_move
+
+        t += 1
+        if t % 10 == 0:  # adjust the speed of falling blocks.
+            y += 1
+            block.figure_sub_squares(x, y, r, typ)
+            if block.check_macro(x, y) or \
+                    block.check_macro(block.base['x1'], block.base['y1']) or \
+                    block.check_macro(block.base['x2'], block.base['y2']) or \
+                    block.check_macro(block.base['x3'], block.base['y3']) is True:
+                # import pdb; pdb.set_trace()
+                y = y - 1
+                block.figure_sub_squares(x, y, r, typ)
+                block.square(x, y, settings.white)
+                block.square(block.base['x1'], block.base['y1'], settings.white)
+                block.square(block.base['x2'], block.base['y2'], settings.white)
+                block.square(block.base['x3'], block.base['y3'], settings.white)
+                block.fill_macro(x, y)
+                block.fill_macro(block.base['x1'], block.base['y1'])
+                block.fill_macro(block.base['x2'], block.base['y2'])
+                block.fill_macro(block.base['x3'], block.base['y3'])
+                while block.is_removable(y):
+                    # import pdb; pdb.set_trace()
+                    block.remove_n_clear(y, settings.black)
+                    block.slide_n_update(y, settings.white, settings.black)
+                while block.is_removable(block.base['y1']):
+                    # import pdb; pdb.set_trace()
+                    block.remove_n_clear(block.base['y1'], settings.black)
+                    block.slide_n_update(block.base['y1'], settings.white, settings.black)
+                while block.is_removable(block.base['y2']):
+                    # import pdb; pdb.set_trace()
+                    block.remove_n_clear(block.base['y2'], settings.black)
+                    block.slide_n_update(block.base['y2'], settings.white, settings.black)
+                while block.is_removable(block.base['y3']):
+                    # import pdb; pdb.set_trace()
+                    block.remove_n_clear(block.base['y3'], settings.black)
+                    block.slide_n_update(block.base['y3'], settings.white, settings.black)
+                x = 5
+                y = 1
+                r = 0
+                typ = random.randrange(0, 7)
+
+        # draw the active block in red.
+        block.figure_sub_squares(x, y, r, typ)
+        block.square(x, y, settings.red)
+        block.square(block.base['x1'], block.base['y1'], settings.red)
+        block.square(block.base['x2'], block.base['y2'], settings.red)
+        block.square(block.base['x3'], block.base['y3'], settings.red)
+
         pygame.display.update()
+
+        # then paint it back to black.
+        block.square(x, y, settings.black)
+        block.square(block.base['x1'], block.base['y1'], settings.black)
+        block.square(block.base['x2'], block.base['y2'], settings.black)
+        block.square(block.base['x3'], block.base['y3'], settings.black)
 
 
 if __name__ == '__main__':
